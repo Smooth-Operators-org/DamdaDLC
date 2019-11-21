59b2ef0a4ec50498e0be12ffb1b2ac77a6524f13
danielrussocki <46513699+danielrussocki@users.noreply.github.com>
2019-11-21 17:32:35 -0500

    bbb
    xdxdxd

---
+++ js/main.js
@@ -0,0 +1,57 @@
+MicroModal.init();
+
+$(document).ready(function(){
+    $('#openFixedMenu').click(function(){
+        $('#fixedMenu').show();
+    });
+    $('#closeFixedMenu').click(function(){
+        $('#fixedMenu').hide();
+    });
+    $('#open-modal-1').click(function(){
+        MicroModal.show('modal-1');
+    });
+    // PRUEBA DE CONEXIÓN A LA API
+    $('#estoEsUnaPrueba').click(function(){
+        let dataPersona = '657489';
+        let urlPost = 'http://localhost:5000/api/users/';
+        let urlDef = urlPost + dataPersona;
+        // $.post(urlDef, function(e){
+        //     console.log(e);
+        // }, "JSON");
+        $.ajax({
+            type: 'GET',
+            crossDomain: true,
+            url: urlDef,
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            responseType: "application/json; charset=utf-8",
+            headers: {
+                'Access-Control-Allow-Credentials':true,
+                'Access-Control-Allow-Origin':'*',
+                'Access-Control-Allow-Methods':'GET',
+                'Access-Control-Allow-Headers':'application/json'
+            },
+            success: function(msg){
+                console.log(msg);
+            }
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function(){
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        plugins: ['dayGrid', 'interaction', 'timeGrid'],
+        header: {
+            left: 'dayGridMonth,timeGridWeek,timeGridDay',
+            right: 'title'
+        },
+        footer: {
+            right: 'prevYear,prev,next,nextYear'
+        }
+    });
+    calendar.on('dateClick', function(info){
+        console.log('clicked on ' + info.dateStr);
+    });
+    calendar.render();
+});
